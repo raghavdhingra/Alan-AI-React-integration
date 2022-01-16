2969f89f2e8bc0787254634082976369e91ed800
Raghav Dhingra <raghav.dhingra15@gmail.com>
2022-01-16 17:44:49 +0530

    updated react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const alanAIRef = React.useRef(null);
+  const [orderList, setOrderList] = React.useState([]);
+
+  React.useEffect(() => {
+    alanAIRef.current = alanBtn({
+      key: process.env.REACT_APP_ALAN_API_KEY,
+      onCommand: function (commandData) {
+        switch (commandData.command) {
+          case "add": {
+            const { item } = commandData;
+            setOrderList((orLst) => {
+              return [...orLst, item];
+            });
+            break;
+          }
+          case "clear": {
+            setOrderList([]);
+            break;
+          }
+          default:
+            break;
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Demo Restaurant</h1>
+      <p>
+        Place Order just by clicking on Alan AI voice button
+        <br />
+        And say "Pizza", "Burger", or any items
+      </p>
+
+      <div className="order-container">
+        {orderList && orderList.length > 0 ? (
+          <>
+            <p>Your Order for</p>
+            <ul className="order-list">
+              {orderList.map((order, index) => (
+                <li className="order-list-item" key={index}>
+                  {order}
+                </li>
+              ))}
+            </ul>
+            <label>have been placed</label>
+          </>
+        ) : (
+          "No order Placed Yet"
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
